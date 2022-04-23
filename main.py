dec9421e85f86c94f073bb33316201f166226f16
Javier Cornejo <javier.cornejo.z@usach.cl>
2022-04-23 18:58:25 -0400

    implementacion checkbuttons para opciones

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 from pyswip import Prolog
-import tkinter
+import tkinter as tk
 prolog = Prolog()
 prolog.consult("baseDeConocimiento.pl")
 
-ventana = tkinter.Tk()
+ventana = tk.Tk()
 ventana.geometry("800x600")
+
+
+opcionDecada = tk.IntVar()
+opcionDuracion = tk.IntVar()
+opcionExperiencia = tk.IntVar()
+
+checkboxesDecada = []
+decadas = ['90','2000','2010','2020']
+for i in range(4):
+    checkboxesDecada.append(tk.Checkbutton(ventana, text = decadas[i],onvalue=i + 1, variable=opcionDecada))  
+    checkboxesDecada[i].pack()
+
+checkboxesDuracion = []
+duraciones = ['Largo','Medio','Corto']
+for j in range (3):
+    checkboxesDuracion.append(tk.Checkbutton(ventana, text = duraciones[j],onvalue=j + 1, variable=opcionDuracion))  
+    checkboxesDuracion[j].pack()
+
+checkboxesExperiencia = []
+experiencias = ['Inexperto','Habil','Experto']
+for k in range(3):
+    checkboxesExperiencia.append(tk.Checkbutton(ventana, text = experiencias[k],onvalue=k + 1, variable=opcionExperiencia))  
+    checkboxesExperiencia[k].pack()
+
+
+
 ventana.mainloop()
 #list(prolog.query("father(michael,X)")) == [{'X': 'john'}, {'X': 'gina'}]
-for soln in prolog.query("juegos(V,'Plataforma',X,Y,Z)"):
-    print(soln["V"])
+#for soln in prolog.query("juegos(V,'Plataforma',X,Y,Z)"):
+#    print(soln["V"])
 
 #juegos(nombre,genero,duracion,categoriaSpeed,decada)
 #generos(genero,experiencia,adicional)
